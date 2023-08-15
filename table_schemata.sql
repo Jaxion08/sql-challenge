c4beadb11da54a861e5928725514c32e03f40519
Jaxion08 <jaxion08@gmail.com>
2023-08-15 15:40:25 -0500

    changed dept_emp to composite key

--- table_schemata.sql
+++ table_schemata.sql
@@ -1,70 +1,71 @@
-CREATE TABLE "Title_id" (
-    "Title_id" VARCHAR(5)   NOT NULL,
-    "Title" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_Title_id" PRIMARY KEY (
-        "Title_id"
+CREATE TABLE "title_id" (
+    "title_id" VARCHAR(5)   NOT NULL,
+    "title" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_title_id" PRIMARY KEY (
+        "title_id"
      )
 );
 
-CREATE TABLE "Employees" (
-    "Emp_no" Integer   NOT NULL,
-    "Emp_title_id" VARCHAR(5)   NOT NULL,
-    "Birth_date" VARCHAR(10)   NOT NULL,
-    "First_name" VARCHAR   NOT NULL,
-    "Last_name" VARCHAR   NOT NULL,
-    "Sex" VARCHAR(1)   NOT NULL,
-    "Hire_date" VARCHAR(10)   NOT NULL,
-    CONSTRAINT "pk_Employees" PRIMARY KEY (
-        "Emp_no"
+CREATE TABLE "employees" (
+    "emp_no" Integer   NOT NULL,
+    "emp_title_id" VARCHAR(5)   NOT NULL,
+    "birth_date" VARCHAR(10)   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "sex" VARCHAR(1)   NOT NULL,
+    "hire_date" VARCHAR(10)   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
      )
 );
 
-CREATE TABLE "Salaries" (
-    "Emp_no" Integer   NOT NULL,
-    "Salary" Integer   NOT NULL,
-    CONSTRAINT "pk_Salaries" PRIMARY KEY (
-        "Emp_no"
+CREATE TABLE "salaries" (
+    "emp_no" Integer   NOT NULL,
+    "salary" Integer   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no"
      )
 );
 
-CREATE TABLE "Departments" (
-    "Dept_no" VARCHAR(4)   NOT NULL,
-    "Dept_name" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_Departments" PRIMARY KEY (
-        "Dept_no"
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR(4)   NOT NULL,
+    "dept_name" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
      )
 );
 
-CREATE TABLE "Dept_emp" (
-    "Emp_no" Integer   NOT NULL,
-    "Dept_no" VARCHAR(4)   NOT NULL,
-    CONSTRAINT "pk_Dept_emp" PRIMARY KEY (
-        "Emp_no"
+CREATE TABLE "dept_emp" (
+    "emp_no" Integer  NOT NULL,
+    "dept_no" VARCHAR(4)   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no",
+        "dept_no"
+    )
+);
+
+CREATE TABLE "dept_manager" (
+    "emp_no" Integer   NOT NULL,
+    "dept_no" VARCHAR(4)   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "emp_no"
      )
 );
 
-CREATE TABLE "Dept_manager" (
-    "Emp_no" Integer   NOT NULL,
-    "Dept_no" VARCHAR(4)   NOT NULL,
-    CONSTRAINT "pk_Dept_manager" PRIMARY KEY (
-        "Emp_no"
-     )
-);
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "title_id" ("title_id");
 
-ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_Emp_title_id" FOREIGN KEY("Emp_title_id")
-REFERENCES "Title_id" ("Title_id");
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
-ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_Emp_no" FOREIGN KEY("Emp_no")
-REFERENCES "Employees" ("Emp_no");
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
-ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_Emp_no" FOREIGN KEY("Emp_no")
-REFERENCES "Employees" ("Emp_no");
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
 
-ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_Dept_no" FOREIGN KEY("Dept_no")
-REFERENCES "Departments" ("Dept_no");
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
-ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_Emp_no" FOREIGN KEY("Emp_no")
-REFERENCES "Employees" ("Emp_no");
-
-ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_Dept_no" FOREIGN KEY("Dept_no")
-REFERENCES "Departments" ("Dept_no");
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
